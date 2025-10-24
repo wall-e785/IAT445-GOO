d6b89df01d23fcc0240aa4e9f8aec47c26d76c12
wall-e785 <wall_echau604@yahoo.ca>
2025-10-23 21:36:03 -0700

    UI Button + Audio Working

---
+++ Assets/Scripts/TutorialScripts/TutorialManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+//manages the home/tutorial scene
+public class TutorialManager : MonoBehaviour
+{
+    public Button startButton;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startButton.onClick.AddListener(OnStartButtonClicked);
+    }
+
+    private void OnStartButtonClicked()
+    {
+        AudioManager.Instance.PlaySound("tutorial-1");
+    }
+}
---
+++ Assets/Scripts/WorldManagers/AudioManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+//Using a singleton pattern, AudioManager enables audio to be played by any other object, accessing the static variable in this script
+//referneced from: https://www.youtube.com/watch?v=y-dNtXTN8qU
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    public AudioClip[] AudioClips;
+
+    public AudioSource AudioSourceTemplate;
+    private AudioSource[] audioSources = new AudioSource[32];
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("Should not have more than 1 AudioManager");
+        }
+
+        for (int i = 0; i < audioSources.Length; ++i)
+        {
+            audioSources[i] = Instantiate(AudioSourceTemplate, transform);
+        }
+    }
+
+    public void PlaySound(string clipName)
+    {
+        AudioSource audioSource = null;
+        AudioClip audioClip = null;
+
+        foreach (AudioSource source in audioSources)
+        {
+            if (!source.isPlaying)
+            {
+                audioSource = source;
+                break;
+            }
+        }
+
+        if (audioSource == null) return;
+
+        foreach (AudioClip clip in AudioClips)
+        {
+            if (clip.name == clipName)
+            {
+                audioClip = clip;
+                break;
+            }
+        }
+
+        if (audioClip == null)
+        {
+            Debug.LogError($"Failed to find sound: {clipName}");
+            return;
+        }
+
+        audioSource.clip = audioClip;
+        audioSource.volume = 1f;
+        audioSource.Play();
+    }
+
+}
