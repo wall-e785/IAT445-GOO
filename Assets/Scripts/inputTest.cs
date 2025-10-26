d5eebfa86f3e9a01bd9a0259c0273d3c86d83eb1
Aden Trathen <130413431+AdenArcAngel@users.noreply.github.com>
2025-10-25 22:00:38 -0700

    Aden Level imported
    There's a problem with playing the level in vr that ill figure out later

---
+++ Assets/Scripts/inputTest.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class inputTest : MonoBehaviour
+{
+    public InputActionProperty testActionValue;
+    public InputActionProperty testActionButton;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float value = testActionValue.action.ReadValue<float>();
+        Debug.Log("VALUE: " + value);
+
+        bool button = testActionButton.action.IsPressed();
+        Debug.Log("BUTTON: " + button); 
+    }
+}
