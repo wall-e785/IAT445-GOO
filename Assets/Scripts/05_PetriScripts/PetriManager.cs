399e5e03a49ae5055243cf4e5d28cff3ef0c6d50
wall-e785 <wall_echau604@yahoo.ca>
2025-11-22 23:55:23 -0800

    Updated Level 1 Sounds + Triggers

--- Assets/Scripts/05_PetriScripts/PetriManager.cs
+++ Assets/Scripts/05_PetriScripts/PetriManager.cs
@@ -29,8 +29,8 @@ public class PetriManager : MonoBehaviour
         if (goo1 == null && goo2 == null && goo3 == null && !done && !introPlaying)
         {
             done = true;
-            AudioManager.Instance.PlaySound("office-2.1");
-            StartCoroutine(playDelay("office-2.2", 8.5f));
+            AudioManager.Instance.PlaySound("BurpSmall");
+            LevelLoader.instance.LoadNextLevel();
         }
 
     }
@@ -38,18 +38,7 @@ public class PetriManager : MonoBehaviour
     IEnumerator waitForIntro()
     {
         yield return new WaitForSeconds(24);
-        UIManager.Instance.setText("Wait for the right moment to escape... It is watching.");
         introPlaying = false;
-    }
-
-    IEnumerator playDelay(string clipName, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        AudioManager.Instance.PlaySound("BurpSmall");
-        AudioManager.Instance.PlaySound(clipName);
-        yield return new WaitForSeconds(5);
-        LevelLoader.instance.LoadNextLevel();
-
     }
 
 }
