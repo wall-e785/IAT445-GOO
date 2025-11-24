106484c41f32a217d12b0cf2bf3be8a12b6ace11
wall-e785 <wall_echau604@yahoo.ca>
2025-11-23 20:57:45 -0800

    Forklift Animation Returned

--- Assets/Scripts/4_SecurityScripts/SecurityBotStun.cs
+++ Assets/Scripts/4_SecurityScripts/SecurityBotStun.cs
@@ -19,8 +19,8 @@ public class SecurityBotStun : MonoBehaviour
                 AudioManager.Instance.PlaySound("Negative");
 
                 //choose random sound to play
-                int soundToPlay = (int)UnityEngine.Random.Range(1, 8);
-                string soundName = "Absorb" + soundToPlay;
+                int soundToPlay = (int)UnityEngine.Random.Range(1, 2);
+                string soundName = "securitybot" + soundToPlay;
                 AudioManager.Instance.PlaySound(soundName);
 
                 Destroy(other.transform.parent.gameObject);
