c1985dfa0a1f3db95e5fa3bbd8376080108cbf9b
Aden Trathen <130413431+AdenArcAngel@users.noreply.github.com>
2025-11-24 23:43:28 -0800

    near far range limited

---
+++ Assets/Scripts/PlayerScripts/ButtonController.cs
@@ -0,0 +1,41 @@
+
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
+
+
+public class ButtonController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private InputDevice leftController;
+    private InputDevice rightController;
+
+    void Start()
+    {
+        leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        if(leftController==null || rightController == null)
+        {
+            Debug.Log("couldn't find controllers");
+        }
+        Debug.Log(leftController.isValid);
+        Debug.Log(rightController.isValid);
+    }
+
+    void Update()
+    {
+        // LEFT HAND (X / Y)
+        if (leftController.TryGetFeatureValue(CommonUsages.primaryButton, out bool xPressed) && xPressed)
+            Debug.Log("X button pressed");
+
+        if (leftController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool yPressed) && yPressed)
+            Debug.Log("Y button pressed");
+
+        // RIGHT HAND (A / B)
+        if (rightController.TryGetFeatureValue(CommonUsages.primaryButton, out bool aPressed) && aPressed)
+            Debug.Log("A button pressed");
+
+        if (rightController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool bPressed) && bPressed)
+            Debug.Log("B button pressed");
+    }
+}
